8d8ec20d0a3034a026898265e0cd06b0a3e42b6d
Prince Tomar <madm47157@gmail.com>
2023-07-11 15:02:23 +0530

    Initial Commit

---
+++ i_am_learning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i_am_learning
+//
+//  Created by Prince Tomar on 11/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ i_am_learningUITests/i_am_learningUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  i_am_learningUITestsLaunchTests.swift
+//  i_am_learningUITests
+//
+//  Created by Prince Tomar on 11/07/23.
+//
+
+import XCTest
+
+final class i_am_learningUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
